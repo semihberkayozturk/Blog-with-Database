068f638e3f6d8b98968dc99df43e4b48def026c6
Semih Berkay Öztürk <ssemihberkay@gmail.com>
2022-06-05 23:50:46 +0300

    add first project and fix footer

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -7,7 +7,7 @@ const _ = require("lodash");
 
 const homeStartingContent = "Hello Everyone I'm Semih, and in this personal website, I do share blog posts about technology. You can also learn more about me and my projects up above. Thank you for your interest !"
 const aboutContent = "Hello ! I'm Semih, and I live in Istanbul/Turkey. Currently I study Management Information Systems at Istanbul Bilgi University. On the other hand, I mostly spend my time working on Back-end Web Development and its components with various materials such as Harvard's CS50, freecodecamp, Postman and so forth.\nIf you want to learn more about me, check out my LinkedIn profile or send me an e-mail. Please free to contact me. Thank You."
-const contactContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing."
+const contactContent = "I've been developing projects with Python, HTML, CSS, JavaScript and Node.js for a while. Take a look at some of my projects and its GitHub links."
 
 const app = express();
 
